d514f883e688506d45f69333e61acd7f0afce278
Jofre Tomas <jofre@acheipneus.com.br>
2025-06-05 18:06:09 -0300

    feat: sub menu das configurações

--- src/app/components/nav-bar/nav-bar.ts
+++ src/app/components/nav-bar/nav-bar.ts
@@ -1,15 +1,35 @@
 import { Component } from '@angular/core';
-import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
-import { MatToolbar } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatRippleModule } from '@angular/material/core'
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [MatIcon],
+  imports: [MatIcon, MatMenuModule, MatRippleModule, FormsModule, MatButtonModule],
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.css'
 })
 export class NavBar {
+    abrirConta(): void {
+    console.log('Item "Conta" clicado');
+  }
 
+  abrirConfiguracoesApp(): void {
+    console.log('Item "Configuração" clicado');
+  }
+
+  fazerLogout(): void {
+    console.log('Item "Sair" clicado');
+  }
+
+    minimizarTela(): void {
+    if ((window as any).api) {
+      (window as any).api.send('minimize-window');
+    } else {
+      console.warn('Funcionalidade de minimizar só está disponível na versão desktop.');
+    }
+  }
 }
