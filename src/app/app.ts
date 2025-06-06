eda7953b77d9a38f1d4d1c3396f23c100d6aceea
Jofre Tomas <jofre@acheipneus.com.br>
2025-06-06 17:39:21 -0300

    feat: finalização do Login

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBar } from './components/nav-bar/nav-bar';
+import { LoginComponent } from "./components/login/login";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBar],
+  imports: [RouterOutlet, NavBar, LoginComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
